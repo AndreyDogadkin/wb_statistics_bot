4e62f6cc3c58f2af6ab50d4c803e9a31fec6a748
Andey Dogadkin <jvgger@gmail.com>
2023-12-18 21:26:45 +0300

    fix exceptions

--- exceptions/wb_exceptions.py
+++ exceptions/wb_exceptions.py
@@ -9,16 +9,6 @@ class WBApiResponseExceptions(Exception):
         return f'Ошибка получения ответа url: {self.url}\n{self.message}'
 
 
-class WBApiHandleException(Exception):
-    """Ошибка обработки ответа от WB API."""
-
-    def __init__(self, message: str = ''):
-        self.message = message
-
-    def __str__(self):
-        return f'Ошибка подготовки ответа.\n Детали: {self.message}'
-
-
 class IncorrectKeyException(Exception):
     """Ошибка 401."""
     pass
