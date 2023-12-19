3525b15b916474eac5b7bf7e183425fa31731c8f
Andey Dogadkin <jvgger@gmail.com>
2023-12-19 17:00:22 +0300

    models refactoring

---
+++ database/models/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    'User',
+    'Token',
+    'FavoriteRequest'
+)
+
+from database.models.user import User
+from database.models.token import Token
+from database.models.favorite import FavoriteRequest
---
+++ database/models/token.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, BLOB
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database.models.base import Base
+
+if TYPE_CHECKING:
+    from database.models.user import User
+
+
+class Token(Base):
+    __tablename__ = 'user_tokens'
+    __table_args__ = ()
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('user_account.telegram_id', ondelete='CASCADE'),
+    )
+    wb_token_content = mapped_column(BLOB, nullable=True)
+    wb_token_analytic = mapped_column(BLOB, nullable=True)
+
+    user: Mapped['User'] = relationship('User', back_populates='tokens')
