cacbc05e54965ccb9105a9443ae8c6e89ff2e990
Andey Dogadkin <jvgger@gmail.com>
2023-12-26 23:24:19 +0300

    Refactor generate_encryption_key.py

---
+++ utils/AES_encryption/generate_encryption_key.py
@@ -0,0 +1,34 @@
+import base64
+
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Random import get_random_bytes
+
+
+def generate_key(password: str, dklen: int, iterations: int):
+    """Сгенерировать ключ шифрования."""
+    bytes_key = PBKDF2(
+        password=password,
+        salt=get_random_bytes(dklen),
+        dkLen=dklen,
+        count=iterations
+    )
+    return base64.b64encode(bytes_key)
+
+
+if __name__ == '__main__':
+    from environs import Env
+
+    env = Env()
+    env.read_env()
+
+    PASSWORD = env('PASSWORD_FOR_ENCRYPTION')
+    DKLEN = env.int('DKLEN')
+    ITERATIONS = env.int('ITERATIONS_FOR_ENCRYPTION')
+
+    print(
+        generate_key(
+            password=PASSWORD,
+            dklen=DKLEN,
+            iterations=ITERATIONS
+        )
+    )
