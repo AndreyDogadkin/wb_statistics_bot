93605f963cf4a1d553fd630254bfc224c6b72766
Andey Dogadkin <jvgger@gmail.com>
2024-01-02 03:38:10 +0300

    Formatted.

--- wb_api/response_handlers.py
+++ wb_api/response_handlers.py
@@ -6,8 +6,14 @@ from pydantic import ValidationError
 from bot.base_messages.messages_templates import err_mess_templates
 from config_data.config import PAGINATION_SIZE
 from exceptions.wb_exceptions import ForUserException
-from wb_api import (ResponseStatsDays, ResponseNmIDs, CardsNmIds, ResponseStatsPeriod,
-                    ConversionsStatsPeriod, BaseResponse)
+from wb_api import (
+    ResponseStatsDays,
+    ResponseNmIDs,
+    CardsNmIds,
+    ResponseStatsPeriod,
+    ConversionsStatsPeriod,
+    BaseResponse
+)
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +26,9 @@ class ResponseHandlers:
         if data.error is True:
             error = data.error_text
             logger.error(f'В ответе присутствуют ошибки: {error}.')
-            raise ForUserException(err_mess_templates['response_error_field_true'])
+            raise ForUserException(
+                err_mess_templates['response_error_field_true']
+            )
 
     @staticmethod
     def __get_datetime_format(_datetime: str, method: str):
@@ -29,7 +37,9 @@ class ResponseHandlers:
             'detail_days': '%Y-%m-%d',
             'detail_period': '%Y-%m-%d %H:%M:%S'
         }
-        return datetime.strptime(_datetime, formats[method]).strftime('%d.%m.%y')
+        return datetime.strptime(
+            _datetime, formats[method]
+        ).strftime('%d.%m.%y')
 
     @classmethod
     def nm_ids_handler(cls, response) -> list[tuple]:
@@ -55,7 +65,9 @@ class ResponseHandlers:
             return out
         except ValidationError as e:
             logger.critical(f'Ошибка валидации ответа: {e.json()}')
-            raise ForUserException(err_mess_templates['response_validation_error'])
+            raise ForUserException(
+                err_mess_templates['response_validation_error']
+            )
 
     @classmethod
     def analytic_detail_days_handler(cls, response):
@@ -68,13 +80,18 @@ class ResponseHandlers:
             out: list = [None] * len_out_list
             out[0] = (data.data[0].imt_name, data.data[0].vendor_code)
             for elem in history:
-                elem.dt = cls.__get_datetime_format(elem.dt, 'detail_days')
+                elem.dt = cls.__get_datetime_format(
+                    elem.dt,
+                    'detail_days'
+                )
                 out[len_out_list - 1] = (elem.__dict__.values())
                 len_out_list -= 1
             return out
         except ValidationError as e:
             logger.critical(f'Ошибка валидации ответа: {e.json()}')
-            raise ForUserException(err_mess_templates['response_validation_error'])
+            raise ForUserException(
+                err_mess_templates['response_validation_error']
+            )
 
     @classmethod
     def analytic_detail_period_handler(cls, response):
@@ -85,20 +102,46 @@ class ResponseHandlers:
             name = data.data.cards[0].object.name
             vendor_code = data.data.cards[0].vendor_code
             statistics = data.data.cards[0].statistics
-            statistics.selected_period.begin = cls.__get_datetime_format(statistics.selected_period.begin,
-                                                                         'detail_period')
-            statistics.selected_period.end = cls.__get_datetime_format(statistics.selected_period.end,
-                                                                       'detail_period')
-            statistics.previous_period.begin = cls.__get_datetime_format(statistics.previous_period.begin,
-                                                                         'detail_period')
-            statistics.previous_period.end = cls.__get_datetime_format(statistics.previous_period.end,
-                                                                       'detail_period')
-            select_period = list(statistics.selected_period.__dict__.values())
-            select_period_buyouts: ConversionsStatsPeriod = select_period.pop()
-            previous_period = list(statistics.previous_period.__dict__.values())
-            previous_period_buyouts: ConversionsStatsPeriod = previous_period.pop()
-            return [(name, vendor_code), ('Выбранный период:', *select_period, select_period_buyouts.buyout_percent),
-                    ('Предыдущий период:', *previous_period, previous_period_buyouts.buyout_percent)]
+            statistics.selected_period.begin = cls.__get_datetime_format(
+                statistics.selected_period.begin,
+                'detail_period'
+            )
+            statistics.selected_period.end = cls.__get_datetime_format(
+                statistics.selected_period.end,
+                'detail_period'
+            )
+            statistics.previous_period.begin = cls.__get_datetime_format(
+                statistics.previous_period.begin,
+                'detail_period')
+            statistics.previous_period.end = cls.__get_datetime_format(
+                statistics.previous_period.end,
+                'detail_period'
+            )
+            select_period = list(
+                statistics.selected_period.__dict__.values()
+            )
+            select_period_buyouts: ConversionsStatsPeriod = (
+                select_period.pop()
+            )
+            previous_period = list(
+                statistics.previous_period.__dict__.values()
+            )
+            previous_period_buyouts: ConversionsStatsPeriod = (
+                previous_period.pop()
+            )
+            return [
+                (name, vendor_code),
+                ('Выбранный период:',
+                 *select_period,
+                 select_period_buyouts.buyout_percent
+                 ),
+                ('Предыдущий период:',
+                 *previous_period,
+                 previous_period_buyouts.buyout_percent
+                 )
+            ]
         except ValidationError as e:
             logger.critical(f'Ошибка валидации ответа: {e.json()}')
-            raise ForUserException(err_mess_templates['response_validation_error'])
+            raise ForUserException(
+                err_mess_templates['response_validation_error']
+            )
