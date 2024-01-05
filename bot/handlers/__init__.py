162ae422246c39069feb8785083ab735df9f88b4
Andey Dogadkin <jvgger@gmail.com>
2024-01-06 02:34:14 +0300

    Added support router to init.

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,7 +3,8 @@ __all__ = (
     'get_stats_router',
     'my_limits_router',
     'save_token_router',
-    'start_help_router'
+    'start_help_router',
+    'support_router'
 )
 
 from bot.handlers.get_favorite_command import get_favorite_router
@@ -11,3 +12,4 @@ from bot.handlers.get_stats_command import get_stats_router
 from bot.handlers.my_limits_command import my_limits_router
 from bot.handlers.save_tokens_command import save_token_router
 from bot.handlers.start_help_commands import start_help_router
+from bot.handlers.support_command import support_router
