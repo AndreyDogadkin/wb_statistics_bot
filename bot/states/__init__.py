cf1e583b26efe0cf7f387d4f1fd8eae4de63958a
Andey Dogadkin <jvgger@gmail.com>
2024-01-06 02:31:15 +0300

    Added help states.

--- bot/states/__init__.py
+++ bot/states/__init__.py
@@ -2,10 +2,12 @@ __all__ = (
     'GetStatsStates',
     'SaveTokenStates',
     'FavoritesStates',
-    'HelpStates'
+    'HelpStates',
+    'SupportStates'
 )
 
 from bot.states.get_favorites import FavoritesStates
 from bot.states.get_stats import GetStatsStates
+from bot.states.help import HelpStates
 from bot.states.save_token import SaveTokenStates
-from bot.states.help_states import HelpStates
+from bot.states.support import SupportStates
