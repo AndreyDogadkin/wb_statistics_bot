fd0c3f70060b440db1c4c98f930367765ff90a04
Andey Dogadkin <jvgger@gmail.com>
2024-01-12 07:18:20 +0300

    Added new database methods.

--- database/methods.py
+++ database/methods.py
@@ -1,6 +1,7 @@
 import datetime
 
-from sqlalchemy import select, Select
+from sqlalchemy import select, Select, exists
+from sqlalchemy.orm import selectinload
 
 from bot.base_messages.messages_templates import get_favorite_message_templates
 from config_data.config import (
@@ -9,7 +10,7 @@ from config_data.config import (
     MAX_LEN_FAVORITES,
 )
 from database import database_connector
-from database.models import User, Token, FavoriteRequest
+from database.models import User, Token, FavoriteRequest, WBAccount
 from exceptions.wb_exceptions import ForUserException
 from utils import AESEncryption
 
@@ -25,20 +26,17 @@ class DBMethods:
         )
         return query
 
-    def __get_query_select_token(
+    def __get_query_select_account(
             self,
             telegram_id: int,
-            content: bool = False,
-            analytic: bool = False
     ) -> Select:
         """Запрос на получение токена."""
-        select_field = Token
-        if content:
-            select_field = Token.wb_token_content
-        elif analytic:
-            select_field = Token.wb_token_analytic
-        query = select(select_field).where(
-            Token.user_id == telegram_id
+        query = select(WBAccount).where(
+            WBAccount.user_id == telegram_id,
+            WBAccount.is_now_active == True  # noqa
+        ).options(
+            selectinload(WBAccount.tokens),
+            selectinload(WBAccount.favorites)
         )
         return query
 
@@ -50,7 +48,10 @@ class DBMethods:
             return user.scalar_one_or_none()
 
     async def add_user_if_not_exist(self, telegram_id: int) -> None:
-        """Добавление пользователя в БД."""
+        """
+        Добавление пользователя такого не существует.
+        Добавление базового аккаунта созданному пользователю.
+        """
         user = await self.get_user(telegram_id)
         if not user:
             async with self.session() as s:
@@ -58,15 +59,92 @@ class DBMethods:
                     telegram_id=telegram_id,
                     last_request=datetime.datetime.now()
                 )
+                user.wb_accounts = [
+                    WBAccount(
+                        user_id=telegram_id,
+                        name=f'Мой магазин',
+                        is_now_active=True
+                    )
+                ]
                 s.add(user)
                 await s.commit()
 
-    async def check_user_token(self, telegram_id: int) -> Token:
+    async def get_user_accounts(self, telegram_id: int):
+        """Получить аккаунты пользователя."""
+        query = select(WBAccount).where(
+            WBAccount.user_id == telegram_id
+        )
+        async with self.session() as s:
+            accounts = await s.execute(query)
+            return accounts.scalars().all()
+
+    async def get_active_account(self, telegram_id) -> WBAccount | None:
+        """Получить активный аккаунт пользователя."""
+        query = select(WBAccount).where(
+            WBAccount.user_id == telegram_id,
+            WBAccount.is_now_active == True
+        )
+        async with self.session() as s:
+            active_account = await s.execute(query)
+            return active_account.scalar_one_or_none()
+
+    async def check_account_name(self, telegram_id, account_name):
+        """Проверка на повторяющиеся имена."""
+        query = exists(WBAccount).where(
+            WBAccount.user_id == telegram_id,
+            WBAccount.name == account_name
+        )
+        async with self.session() as s:
+            account = await s.execute(query)
+            return account.scalar()
+
+    async def create_user_account(self, telegram_id, account_name):
+        """Создать аккаунт пользователя."""
+        async with self.session() as s:
+            account = WBAccount(
+                user_id=telegram_id,
+                name=account_name
+            )
+            s.add(account)
+            await s.commit()
+
+    async def change_account_name(
+            self,
+            telegram_id: int,
+            account_id: int,
+            new_name: str,
+    ) -> str | None:
+        """Изменить имя аккаунта."""
+        query = select(WBAccount).where(
+            WBAccount.id == account_id,
+            WBAccount.user_id == telegram_id,
+        )
+        async with self.session() as s:
+            account = await s.execute(query)
+            account.scalar_one_or_none()
+            if account:
+                account.name = new_name
+                s.commit()
+                return new_name
+
+    async def change_active_account(self, telegram_id: int):
+        """Изменить активный аккаунт пользователя."""
+        pass
+
+    async def delete_account(self, telegram_id):
+        """Удалить аккаунт пользователя."""
+        pass
+
+    async def __get_user_token(
+            self,
+            telegram_id: int
+    ) -> tuple[Token, WBAccount]:
         """Проверка наличия токенов у пользователя."""
         async with self.session() as s:
-            query = self.__get_query_select_token(telegram_id)
-            token = await s.execute(query)
-        return token.scalar_one_or_none()
+            query = self.__get_query_select_account(telegram_id)
+            account = await s.execute(query)
+            account = account.scalar_one_or_none()
+            return account.tokens, account.id
 
     async def save_content_token(
             self,
@@ -79,13 +157,13 @@ class DBMethods:
         )
         encrypted_token = tokens_dict.get('token_content')
         async with self.session() as s:
-            token = await self.check_user_token(telegram_id)
+            token, account_id = await self.__get_user_token(telegram_id)
             if token:
                 s.add(token)
                 token.wb_token_content = encrypted_token
             else:
                 token = Token(
-                    user_id=telegram_id,
+                    wb_account_id=account_id,
                     wb_token_content=encrypted_token,
                 )
                 s.add(token)
@@ -102,13 +180,13 @@ class DBMethods:
         )
         encrypted_token = tokens_dict.get('token_analytic')
         async with self.session() as s:
-            token = await self.check_user_token(telegram_id)
+            token, account_id = await self.__get_user_token(telegram_id)
             if token:
                 s.add(token)
                 token.wb_token_analytic = encrypted_token
             else:
                 token = Token(
-                    user_id=telegram_id,
+                    wb_account_id=account_id,
                     wb_token_analytic=encrypted_token,
                 )
                 s.add(token)
@@ -116,38 +194,44 @@ class DBMethods:
 
     async def get_user_content_token(self, telegram_id: int) -> str | None:
         """Получение токена типа 'Контент'."""
+        query = self.__get_query_select_account(telegram_id)
         async with self.session.begin() as s:
-            query = self.__get_query_select_token(telegram_id, content=True)
-            token = await s.execute(query)
-            token = token.scalar_one_or_none()
-            if token:
+            account = await s.execute(query)
+            account = account.scalar_one_or_none()
+            token = account.tokens
+            if token and token.wb_token_content:
                 tokens_dict: dict = AESEncryption.encrypt_keys(
                     decrypt=True,
-                    token_content=token
+                    token_content=token.wb_token_content
                 )
                 return tokens_dict.get('token_content')
 
     async def get_user_analytic_token(self, telegram_id: int) -> str | None:
         """Получение токена типа 'Аналитика'."""
+        query = self.__get_query_select_account(telegram_id)
         async with self.session.begin() as s:
-            query = self.__get_query_select_token(telegram_id, analytic=True)
-            token = await s.execute(query)
-            token = token.scalar_one_or_none()
-            if token:
+            account = await s.execute(query)
+            account = account.scalar_one_or_none()
+            token = account.tokens
+            if token and token.wb_token_analytic:
                 tokens_dict: dict = AESEncryption.encrypt_keys(
                     decrypt=True,
-                    token_analytic=token
+                    token_analytic=token.wb_token_analytic
                 )
                 return tokens_dict.get('token_analytic')
 
     async def get_user_tokens(self, telegram_id: int) -> dict[str: str] | None:
         """Получить токены пользователя."""
-        query = select(Token).where(
-            Token.user_id == telegram_id
+        query = select(WBAccount).where(
+            WBAccount.is_now_active == True,
+            WBAccount.user_id == telegram_id
+        ).options(
+            selectinload(WBAccount.tokens)
         )
         async with self.session() as s:
-            decrypted_tokens = await s.execute(query)
-            decrypted_tokens = decrypted_tokens.scalar_one_or_none()
+            account = await s.execute(query)
+            account = account.scalar_one_or_none()
+            decrypted_tokens = account.tokens
             if (decrypted_tokens and decrypted_tokens.wb_token_content
                     and decrypted_tokens.wb_token_analytic):
                 encrypted_tokens = AESEncryption.encrypt_keys(
@@ -209,13 +293,13 @@ class DBMethods:
 
     async def check_favorite(
             self,
-            telegram_id: int,
+            account_id: int,
             nm_id: int,
             period: int
     ) -> FavoriteRequest | None:
         """Проверить наличие запроса пользователя в избранном."""
         query = select(FavoriteRequest).where(
-            FavoriteRequest.user_id == telegram_id,
+            FavoriteRequest.wb_account_id == account_id,
             FavoriteRequest.nm_id == nm_id,
             FavoriteRequest.period == period,
         )
@@ -224,15 +308,14 @@ class DBMethods:
             favorite = favorite.scalar_one_or_none()
             return favorite
 
-    async def check_limit_favorite(self, telegram_id: int) -> bool:
+    async def check_limit_favorite(self, telegram_id: int) -> tuple[bool, int]:
         """Проверить лимит на добавление в избранное."""
-        query = select(FavoriteRequest).where(
-            FavoriteRequest.user_id == telegram_id
-        )
+        query = self.__get_query_select_account(telegram_id=telegram_id)
         async with self.session() as s:
-            favorites = await s.execute(query)
-            len_favorites = len(favorites.scalars().all())
-            return len_favorites < MAX_LEN_FAVORITES
+            account = await s.execute(query)
+            account = account.scalar_one_or_none()
+            len_favorites = len(account.favorites)
+            return len_favorites < MAX_LEN_FAVORITES, account.id
 
     async def add_favorite_request(
             self,
@@ -243,8 +326,10 @@ class DBMethods:
             photo_url: str
     ) -> None:
         """Добавить запрос в избранное."""
-        favorite = await self.check_favorite(telegram_id, nm_id, period)
-        limit_favorites = await self.check_limit_favorite(telegram_id)
+        limit_favorites, account_id = await self.check_limit_favorite(
+            telegram_id
+        )
+        favorite = await self.check_favorite(account_id, nm_id, period)
         if not favorite:
             if not limit_favorites:
                 raise ForUserException(
@@ -255,7 +340,7 @@ class DBMethods:
             async with self.session() as s:
                 favorite = FavoriteRequest(
                     name=name,
-                    user_id=telegram_id,
+                    wb_account_id=account_id,
                     nm_id=nm_id,
                     period=period,
                     photo_url=photo_url
@@ -268,12 +353,11 @@ class DBMethods:
             telegram_id: int
     ) -> list[FavoriteRequest]:
         """Получить все избранные запросы пользователя."""
-        query = select(FavoriteRequest).where(
-            FavoriteRequest.user_id == telegram_id
-        )
+        query = self.__get_query_select_account(telegram_id=telegram_id)
         async with self.session() as s:
-            favorites = await s.execute(query)
-            favorites = favorites.scalars().all()
+            account = await s.execute(query)
+            account = account.scalar_one_or_none()
+            favorites = account.favorites
             return favorites
 
     async def delete_user_favorite(
@@ -283,8 +367,9 @@ class DBMethods:
             period: int
     ):
         """Удалить избранный запрос пользователя."""
+        account = await self.get_active_account(telegram_id)
         query = select(FavoriteRequest).where(
-            FavoriteRequest.user_id == telegram_id,
+            FavoriteRequest.wb_account_id == account.id,
             FavoriteRequest.nm_id == nm_id,
             FavoriteRequest.period == period
         )
@@ -294,7 +379,7 @@ class DBMethods:
             await s.delete(favorite)
             await s.commit()
 
-    async def delete_user_account(self, telegram_id):
+    async def delete_user(self, telegram_id):
         """Удаление пользователя."""
         query = self.__get_query_select_user(telegram_id)
         async with self.session() as s:
