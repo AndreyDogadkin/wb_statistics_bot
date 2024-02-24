5a9e0de337574eeb7606780c74e010dad4d2bf3a
Andey Dogadkin <jvgger@gmail.com>
2024-02-24 23:19:01 +0300

    Delete helpers package.

--- bot/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = (
-    'get_user_statistics',
-    'to_update_limits_format',
-)
-
-from bot.helpers.get_user_statistics import get_user_statistics
-from bot.helpers.users_limits import to_update_limits_format
