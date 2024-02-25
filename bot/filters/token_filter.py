f89091e5cee138bbf0fa098a1afb95b01bfce007
Andey Dogadkin <jvgger@gmail.com>
2024-02-26 00:32:23 +0300

    Fix token filter.

--- bot/filters/token_filter.py
+++ bot/filters/token_filter.py
@@ -3,6 +3,5 @@ from aiogram.filters import BaseFilter
 
 
 class TokenFilter(BaseFilter):
-
     async def __call__(self, message: types.Message):
         return 360 <= len(message.text) and message.text.startswith('eyJhb')
