ddfc61cc2f2f6e9ce50b4465ec57e217346aa94b
Andey Dogadkin <jvgger@gmail.com>
2024-05-19 11:51:07 +0300

    Fixed middleware.

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -3,11 +3,11 @@ __all__ = ('AuthMiddleware', 'set_middleware', 'ThrottlingMiddleware')
 from aiogram import Dispatcher
 
 from bot.middlewares.auth import AuthMiddleware
-from bot.middlewares.throttling import ThrottlingMiddleware
 from bot.middlewares.logging import LoggingMiddleware
+from bot.middlewares.throttling import ThrottlingMiddleware
 
 
 def set_middleware(dp: Dispatcher):
-    dp.message.middleware(AuthMiddleware())
+    dp.message.outer_middleware(AuthMiddleware())
     dp.callback_query.middleware(ThrottlingMiddleware())
     dp.update.outer_middleware(LoggingMiddleware())
